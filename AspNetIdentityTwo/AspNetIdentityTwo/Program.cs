978a1d6349703a08d9b68c07e3790cf2c2f89cf7
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2018-01-08 00:01:29 -0500

    Playing with Identity

---
+++ AspNetIdentityTwo/AspNetIdentityTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspNetIdentityTwo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string userName = "utest@belatrixsf.com";
+            string password = "Hamtaro@2017";
+
+
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var creationRes = userManager.Create(new IdentityUser(userName), password);
+
+            Console.WriteLine("User created : {0}", creationRes);
+        }
+    }
+}
